01c449d09c8f9ab5da557b77ef40cbd9b35a95b9
JeansLaw <32730817+JeansLaw@users.noreply.github.com>
2019-09-19 18:23:50 +0800

    Update testCos.py

--- testCos.py
+++ testCos.py
@@ -2,7 +2,7 @@ import os
 
 from util import File
 
-testpath = r"D:\work\批批网\v6.5\色情暴恐广告监测测试数据"
+testpath = r"D:\work\色情暴恐广告监测测试数据"
 
 fileUtil=File.ReadFile()
 fileList=fileUtil.renameFile(testpath)
